d934a38640810e2f4cd1ee341038078938f78218
MariaMarusakova <maruska.marusakova@gmail.com>
2024-04-07 18:59:27 +0200

    final design

--- app/page.js
+++ app/page.js
@@ -1,113 +1,77 @@
-import Image from "next/image";
+'use client'
+import { Switch } from "@/components/ui/switch"
+import { useState } from 'react'
+import Image from 'next/image'
 
 export default function Home() {
+  const [isChecked, setChecked] = useState(false)
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+
+    
+    <main className="flex md:h-screen flex-col items-center justify-between md:p-2 p-4 bg-no-repeat bg-opacity-10 bg-LightGrayishBlue" style={{backgroundImage: "url('/bg-bottom.svg'),url('/bg-top.svg')", backgroundPosition: "left 400px bottom 200px, right 300px top 10px"}} >
+      <div className="relative block place-items-center mt-4 md:mt-28 md:mb-20">
+
+      <h1 className="mb-4 text-center text-2xl font-bold text-GrayishBlue md:-ml-11">Our Pricing</h1>
+      <div className="place-items-center flex mb-4 ml-6 md:mb-20 md:ml-72">      
+          <label className="pr-4 text-PurpleGradientStart text-xs font-bold">Annually</label>
+          <Switch id="annuallyvsmonthly" checked={isChecked} onCheckedChange={setChecked}/>
+          <label className="pl-4 text-PurpleGradientStart text-xs font-bold">Monthly</label>
+      </div>
+      <div className="md:flex grid place-items-center text-center">
+
+        <div className="h-[330px] w-64 md:w-72 bg-white rounded-lg lg:rounded-l-lg lg:rounded-none mt-2 text-GrayishBlue mb-4">
+          <h2 className="mt-7 font-bold text-sm text-GrayishBlue">Basic</h2>
+          <div className="grid grid-cols-1 divide-y mb-2 pr-8 pl-8 pt-4 px-4 py-4">
+          <p className="text-3xl mb-4 mt-4 font-bold text-DarkGrayishBlue"><span className="align-[4px]">$ </span>
+          <span className={(!isChecked)?"monthly mb-4 mt-4 font-bold text-5xl text-DarkGrayishBlue" : "monthly hidden mb-4 mt-4 font-bold text-5xl text-DarkGrayishBlue"} id="monthly">19.99</span>
+          <span className={(isChecked)?"yearly mb-8 mt-7 font-bold text-5xl text-DarkGrayishBlue":"yearly hidden mb-8 mt-7 font-bold text-5xl text-DarkGrayishBlue"} id="yearly">199.99</span></p>          
+          <p className="p-2 text-sm font-bold">500 GB Storage</p>
+          <p className="p-2 text-sm font-bold">2 Users Allowed</p>
+          <p className="p-2 text-sm font-bold">Send up to 3 GB</p>
+          <p></p>
+          </div>
+          <button className="rounded-md bg-gradient-to-r from-PurpleGradientStart to-PurpleGradientEnd text-white font-bold text-xs px-2 py-2 pr-14 pl-14 
+          hover:border-GrayishBlue hover:text-PurpleGradientEnd  hover:from-white hover:to-white hover:border-solid hover:border-[1px]" >LEARN MORE</button>
         </div>
+
+
+        <div className="md:h-[380px] h-[330px] w-64 md:w-64 bg-gradient-to-r from-PurpleGradientStart to-PurpleGradientEnd rounded-lg mb-4 text-white">
+            <h2 className="mt-7 font-bold text-sm text-white">Master</h2>
+                <div className="grid grid-cols-1 divide-y mb-2 pr-8 pl-8 pt-4 px-4 py-4">
+                <p className="text-3xl mb-4 mt-4 font-bold text-white"><span className="align-[4px]">$ </span>
+                <span className={(!isChecked)?"monthly mb-4 mt-4 font-bold text-5xl text-white" : "monthly hidden mb-4 mt-4 font-bold text-5xl text-white"} id="monthly">39.99</span>
+                <span className={(isChecked)?"yearly mb-8 mt-7 font-bold text-5xl text-white":"yearly hidden mb-8 mt-7 font-bold text-5xl text-white"} id="yearly">399.99</span></p>          
+                <p className="p-2 text-sm font-bold">2 TB Storage</p>
+                <p className="p-2 text-sm font-bold">10 Users Allowed</p>
+                <p className="p-2 text-sm font-bold">Send up to 20 GB</p>
+                <p></p>
+                </div>
+                <button className="rounded-md bg-gradient-to-r hover:from-PurpleGradientStart hover:to-PurpleGradientEnd text-PurpleGradientEnd font-bold text-xs px-2 py-2 pr-14 pl-14 
+                hover:border-white hover:text-white  from-white to-white hover:border-solid hover:border-[1px]" >LEARN MORE</button>       
+        </div>
+
+
+        <div className="h-[330px] w-64 md:w-72 bg-white rounded-lg lg:rounded-r-lg lg:rounded-none mt-2 text-GrayishBlue mb-4">
+          <h2 className="mt-7 font-bold text-sm text-GrayishBlue">Master</h2>
+            <div className="grid grid-cols-1 divide-y mb-2 pr-8 pl-8 pt-4 px-4 py-4">
+            <p className="text-3xl mb-4 mt-4 font-bold text-DarkGrayishBlue"><span className="align-[4px]">$ </span>
+            <span className={(!isChecked)?"monthly mb-4 mt-4 font-bold text-5xl text-DarkGrayishBlue" : "monthly hidden mb-4 mt-4 font-bold text-5xl text-DarkGrayishBlue"} id="monthly">39.99</span>
+            <span className={(isChecked)?"yearly mb-8 mt-7 font-bold text-5xl text-DarkGrayishBlue":"yearly hidden mb-8 mt-7 font-bold text-5xl text-DarkGrayishBlue"} id="yearly">399.99</span></p>          
+            <p className="p-2 text-sm font-bold">2 TB Storage</p>
+            <p className="p-2 text-sm font-bold">10 Users Allowed</p>
+            <p className="p-2 text-sm font-bold">Send up to 20 GB</p>
+            <p></p>
+            </div>
+            <button className="rounded-md bg-gradient-to-r from-PurpleGradientStart to-PurpleGradientEnd text-white font-bold text-xs px-2 py-2 pr-14 pl-14 
+            hover:border-GrayishBlue hover:text-PurpleGradientEnd  hover:from-white hover:to-white hover:border-solid hover:border-[1px]" >LEARN MORE</button>
+        </div>
+
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
+
   );
+
 }
